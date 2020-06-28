471afc6f838f95b2c4c1f7905c5e90c23675612a
Afirehong <varhong2018@gmail.com>
2020-06-28 18:33:23 +0800

    用户修改密码

--- php/personAPI.php
+++ php/personAPI.php
@@ -2,7 +2,7 @@
 /*
  * @Author: your name
  * @Date: 2020-06-27 21:00:35
- * @LastEditTime: 2020-06-27 22:36:04
+ * @LastEditTime: 2020-06-28 18:29:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \books_management\php\personAPI.php
@@ -38,6 +38,36 @@ switch ($op) {
         }else{
             $responseData['code'] = 1;
             $responseData['msg'] = '更新个人信息失败';
+            echo json_encode($responseData);
+        }
+        break;
+    case 'modifyPwd':
+        if (empty($_SESSION['uid'])) {
+            die(json_encode(array('code' => -1, 'msg' => '请登录')));
+        }
+        $data = file_get_contents("php://input");
+        $data = json_decode($data);
+        $nowpassword = md5($data->nowpassword);
+        $afterpassword = md5($data->afterpassword);
+        $sql = "SELECT password FROM user WHERE user_id = '{$_SESSION['uid']}'";
+        $res = ($mySQLi->query($sql))->fetch_assoc();
+        if(strcmp($res['password'], $nowpassword)==0){
+            if(strcmp($afterpassword, $res['password'])==0){
+                echo json_encode(array('code' => 1,'msg' => '新旧密码不能相同' ));
+                exit;
+            }
+            $sql = "UPDATE user SET password = '{$afterpassword}' WHERE user_id = '{$_SESSION['uid']}'";
+            if($mySQLi->query($sql)){
+                $responseData['msg'] = '修改成功';
+                echo json_encode($responseData);
+            }else{
+                $responseData['code'] = -1;
+                $responseData['msg'] = '修改失败';
+                echo json_encode($responseData);
+            }
+        }else{
+            $responseData['code'] = 1;
+            $responseData['msg'] = '当前密码错误';
             echo json_encode($responseData);
         }
         break;
