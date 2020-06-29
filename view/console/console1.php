25c073830a56eb91d5eae12da722edd4520eac69
Afirehong <varhong2018@gmail.com>
2020-06-29 13:21:00 +0800

    修改

--- view/console/console1.php
+++ view/console/console1.php
@@ -17,7 +17,7 @@ $cate_nums = $res2[0]['num'];
 
 <head>
 	<meta charset="utf-8">
-	<title>首页三</title>
+	<title>首页</title>
 	<meta name="renderer" content="webkit">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
